de4f0f1bce947178c794dbb176671c39f931b8c3
Ricardo <chirinos.r2908@gmail.com>
2025-06-07 01:09:32 -0400

    feat(routing): agregar enrutamiento principal con carga modular

---
+++ src/app/features/doctores/doctores.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DoctoresRoutingModule } from './doctores-routing.module';
+import { DoctorsComponent } from './doctors.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DoctoresRoutingModule
+  ]
+})
+export class DoctoresModule { }
